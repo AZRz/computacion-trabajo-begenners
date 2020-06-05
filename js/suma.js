c32ed773b1e82016191331114d559f21a7006cca
AZRz <66448010+AZRz@users.noreply.github.com>
2020-06-04 19:05:27 -0500

    Add files via upload

---
+++ js/suma.js
@@ -0,0 +1,35 @@
+alert("Desayunos")
+var ame = prompt("Cuantos platillos de Combinacion americana piensa consumir (cuestan $60.00 pesos MXN)");
+document.write("</p>Costo de los chilaquiles: " + "$"+
+ + 60*ame+"  Pesos MXN");
+
+var bow = prompt("Cuantos platillos de bowl de frutas piensa consumir (cuestan $30.00 pesos MXN)");
+document.write("</p>Costo de los HotCakes: " + "$"+
+ + 30*bow+"  Pesos MXN");
+
+var hue = prompt("Cuantos platillos de huevos estrellados piensa consumir (cuestan $70.00 pesos MXN)");
+document.write("</p>Costo de los Molletes: " + "$"+
+ + 70*hue+"  Pesos MXN");
+
+ alert("comidas")
+var cha = prompt("Cuantos platillos de Chamorro piensa consumir (cuestan $70.00 pesos MXN)");
+document.write("</p>Costo de las Pechuga: " + "$"+
+ + 70*cha+"  Pesos MXN");
+
+var alb = prompt("Cuantos platillos de Albondigas piensa consumir (cuestan $50.00 pesos MXN)");
+document.write("</p>Costo de las Milanesas: " + "$"+
+ + 50*alb+"  Pesos MXN");
+
+var car = prompt("Cuantos platillos de Carne asada piensa consumir (cuestan $50.00 pesos MXN)");
+document.write("</p>Costo de los Pozoles: " + "$"+
+ + 50*car+"  Pesos MXN");
+
+alert("especialidades")
+
+var po = prompt("Cuantos platillos de Pozole piensa consumir (cuestan $50.00 pesos MXN)");
+document.write("</p>Costo de los Pozoles: " + "$"+
+ + 50*po+"  Pesos MXN");
+
+ var ta = prompt("Cuantos platillos de Tacos piensa consumir (cuestan $40.00 pesos MXN)");
+document.write("</p>Costo de los Pozoles: " + "$"+
+ + 40*ta+"  Pesos MXN");
